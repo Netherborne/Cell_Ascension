989a50d8aea7c632c4176b90fadb11f59e6df0e1
Netherborne <kiriazos2@gmail.com>
2026-07-30 04:02:06 +0300

    fix pet frame not showing when solo

--- RaidFrames/Groups/SoloFrame.lua
+++ RaidFrames/Groups/SoloFrame.lua
@@ -57,19 +57,23 @@ local function SoloFrame_UpdateLayout(layout, which)
 
     if not which or which == "main-arrangement" or which == "pet-arrangement" then
         petButton:ClearAllPoints()
-        if layout["main"]["orientation"] == "vertical" then
+        
+        local orientation = layout["pet"]["sameArrangementAsMain"] and layout["main"]["orientation"] or layout["pet"]["orientation"]
+        local anchor = layout["pet"]["sameArrangementAsMain"] and layout["main"]["anchor"] or layout["pet"]["anchor"]
+
+        if orientation == "vertical" then
             -- anchor
             local point, anchorPoint
             local petSpacing = layout["pet"]["sameArrangementAsMain"] and layout["main"]["spacingY"] or layout["pet"]["spacingY"]
 
-            if layout["main"]["anchor"] == "BOTTOMLEFT" then
+            if anchor == "BOTTOMLEFT" then
                 point, anchorPoint = "BOTTOMLEFT", "TOPLEFT"
-            elseif layout["main"]["anchor"] == "BOTTOMRIGHT" then
+            elseif anchor == "BOTTOMRIGHT" then
                 point, anchorPoint = "BOTTOMRIGHT", "TOPRIGHT"
-            elseif layout["main"]["anchor"] == "TOPLEFT" then
+            elseif anchor == "TOPLEFT" then
                 point, anchorPoint = "TOPLEFT", "BOTTOMLEFT"
                 petSpacing = -petSpacing
-            elseif layout["main"]["anchor"] == "TOPRIGHT" then
+            elseif anchor == "TOPRIGHT" then
                 point, anchorPoint = "TOPRIGHT", "BOTTOMRIGHT"
                 petSpacing = -petSpacing
             end
@@ -80,14 +84,14 @@ local function SoloFrame_UpdateLayout(layout, which)
             local point, anchorPoint
             local petSpacing = layout["pet"]["sameArrangementAsMain"] and layout["main"]["spacingX"] or layout["pet"]["spacingX"]
 
-            if layout["main"]["anchor"] == "BOTTOMLEFT" then
+            if anchor == "BOTTOMLEFT" then
                 point, anchorPoint = "BOTTOMLEFT", "BOTTOMRIGHT"
-            elseif layout["main"]["anchor"] == "BOTTOMRIGHT" then
+            elseif anchor == "BOTTOMRIGHT" then
                 point, anchorPoint = "BOTTOMRIGHT", "BOTTOMLEFT"
                 petSpacing = -petSpacing
-            elseif layout["main"]["anchor"] == "TOPLEFT" then
+            elseif anchor == "TOPLEFT" then
                 point, anchorPoint = "TOPLEFT", "TOPRIGHT"
-            elseif layout["main"]["anchor"] == "TOPRIGHT" then
+            elseif anchor == "TOPRIGHT" then
                 point, anchorPoint = "TOPRIGHT", "TOPLEFT"
                 petSpacing = -petSpacing
             end
@@ -98,11 +102,15 @@ local function SoloFrame_UpdateLayout(layout, which)
 
     if not which or which == "pet" then
         if layout["pet"]["soloEnabled"] then
-            RegisterAttributeDriver(petButton, "state-visibility", "[nopet] hide; [vehicleui] hide; show")
+            if not InCombatLockdown() then
+                UnregisterAttributeDriver(petButton, "state-visibility")
+                RegisterUnitWatch(petButton)
+            end
         else
             if InCombatLockdown() then
                 RegisterAttributeDriver(petButton, "state-visibility", "hide")
             else
+                UnregisterUnitWatch(petButton)
                 UnregisterAttributeDriver(petButton, "state-visibility")
                 petButton:Hide()
             end
